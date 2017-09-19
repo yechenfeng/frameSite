43f1ffbefd9edeb316ca12787459b3b29ac01205
howbuy2017 <15000001404@163.com>
2017-09-19 19:11:35 +0800

    格式整理

--- others/node/node-demo/index.js
+++ others/node/node-demo/index.js
@@ -1,123 +1,129 @@
 // 🍎🍎🍎🍎🍎🍎🍎🍎🍎 01 Node.js 起步 🍎🍎🍎🍎🍎🍎🍎🍎🍎<br />
 // 🍠 【准备 Node.js】<br />
 // 🍎🍎🍎 管理安装在系统上的 Node.js<br />
-// LTS  长期支持版本
-// current  当前最新版本
-// Docker   下载不同node的镜像
-// nvm  管理node，自由切换
-    // brew install nvm
-    // [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh  # This loads NVM
-    // nvm ls-remote(列出当前网上所有node版本)
-    // 安装
-        // nvm install v8.3.0(安装指定版本)
-        // nvm install --lts(安装最新稳定版本)
-        // nvm install node(安装最新版本)
-    // nvm ls(查看系统安装的所有node)
-    // node -v
-    // nvm use v6.11.0(切换node版本)
+        // LTS  长期支持版本
+        // current  当前最新版本
+        // Docker   下载不同node的镜像
+        // nvm  管理node，自由切换
+            // brew install nvm
+            // [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh  # This loads NVM
+            // nvm ls-remote(列出当前网上所有node版本)
+            // 安装
+                // nvm install v8.3.0(安装指定版本)
+                // nvm install --lts(安装最新稳定版本)
+                // nvm install node(安装最新版本)
+            // nvm ls(查看系统安装的所有node)
+            // node -v
+            // nvm use v6.11.0(切换node版本)
 // 🍎🍎🍎 创建 Node.js 项目<br />
-// npm init(创建package.son文件)
-// npm init -y(创建使用默认配置的package.json文件)
-// rm -rf package.json(删除文件)
-// npm install -g yarn(facebook替代npm的工具)
-    // yarn init(创建package.son文件)
-console.log('hello node ~');
+        // npm init(创建package.son文件)
+        // npm init -y(创建使用默认配置的package.json文件)
+        // rm -rf package.json(删除文件)
+        // npm install -g yarn(facebook替代npm的工具)
+            // yarn init(创建package.son文件)
+        console.log('hello node ~');
 // 🍎🍎🍎 使用不同版本的 Node.js<br />
-// nvm run v6.11.0 index.js(使用此版本运行js)
-// nvm exec v6.11.0 node -v(使用此版本执行命令)
-// 使用文件.nvmrc
-    // nvm run node -v(使用.nvmrc中的node配置版本执行命令)
-// nvm use(切换node版本为.nvmrc中的node配置版本执行命令)
-// 🍠 【使用模块】<br />
+        // nvm run v6.11.0 index.js(使用此版本运行js)
+        // nvm exec v6.11.0 node -v(使用此版本执行命令)
+        // 使用文件.nvmrc
+            // nvm run node -v(使用.nvmrc中的node配置版本执行命令)
+        // nvm use(切换node版本为.nvmrc中的node配置版本执行命令)
+        // 🍠 【使用模块】<br />
 // 🍎🍎🍎 使用内置的 Node.js 模块<br />
-const os = require('os');
-console.log(os.hostname());
-// 🍎🍎🍎 安装第三方 Node.js 模块<br />
-// 例如，安装http请求模块
-// npm install request --save
-// 或
-// yarn add request
-    // modules有很多依赖，所以会有很多模块
+        const os = require('os');
+        console.log(os.hostname());
+        // 🍎🍎🍎 安装第三方 Node.js 模块<br />
+        // 例如，安装http请求模块
+        // npm install request --save
+        // 或
+        // yarn add request
+            // modules有很多依赖，所以会有很多模块
 // 🍎🍎🍎 使用第三方 Node.js 模块<br />
-const request = require('request');
-// request({
-//     url: 'http://api.douban.com/v2/movie/top250',
-//     json: true
-// }, (error, response, body) => {
-//     console.log(JSON.stringify(body, null, 2));
-// });
+        const request = require('request');
+        // request({
+        //     url: 'http://api.douban.com/v2/movie/top250',
+        //     json: true
+        // }, (error, response, body) => {
+        //     console.log(JSON.stringify(body, null, 2));
+        // });
 // 🍎🍎🍎 创建与使用自定义 Node.js 模块<br />
-const greeting = require('./src/greeting');
-greeting.hello();
+        const greeting = require('./src/greeting');
+        greeting.hello();
 // 🍠 【项目】<br />
 // 🍎🍎🍎 nodemon：监视应用的变化自动重启应用<br />
-// 安装监视模块（模式：开发依赖）
-    // yarn add nodemon --dev
-// 监视文件，直观写法
-    // ./node_modules/.bin/nodemon index.js
-// 初始化，简易写法
-    // "scripts": {
-        // "start": "./node_modules/.bin/nodemon index.js"
-    // },
-// 初始化，简易用法
-// npm start
+        // 安装监视模块（模式：开发依赖）
+            // yarn add nodemon --dev
+        // 监视文件，直观写法
+            // ./node_modules/.bin/nodemon index.js
+        // 初始化，简易写法
+            // "scripts": {
+                // "start": "./node_modules/.bin/nodemon index.js"
+            // },
+        // 初始化，简易用法
+        // npm start
 // 🍎🍎🍎 Node.js 项目的版本控制<br />
-// 安装 package.json 中的工具
-    // npm install
-// 安装 yarn.lock 中的工具
-    // yarn install
-// 版本控制
-    // git init
-    // 添加忽略文件列表（不提交至仓库） .gieignore
-    // git add .
-    // git status
-    // git commit -m 'init'
-//
+        // 安装 package.json 中的工具
+            // npm install
+        // 安装 yarn.lock 中的工具
+            // yarn install
+        // 版本控制
+            // git init
+            // 添加忽略文件列表（不提交至仓库） .gieignore
+            // git add .
+            // git status
+            // git commit -m 'init'
+// 
 // 🍌🍌🍌🍌🍌🍌🍌🍌🍌 02 Node.js 核心模块 🍌🍌🍌🍌🍌🍌🍌🍌🍌<br />
 // 🍠 【准备】<br />
 // 🍌🍌🍌 准备<br />
-// npm start
+        // npm start
 // 🍠 【Events】<br />
 // 🍌🍌🍌 Events<br />
-// 发布订阅模式
-// 电视剧有更新，用户收到通知，决定是否观看
+        // 发布订阅模式
+        // 电视剧有更新，用户收到通知，决定是否观看
 // 🍌🍌🍌 使用事件：EventEmitter<br />
-const EventEmitter = require('events');
-class Player extends EventEmitter {};
-var player = new Player();
-player.on('play', () => { // play 就是事件
-    console.log('正在播放');
-});
-player.emit('play');
+        const EventEmitter = require('events');
+        class Player extends EventEmitter {};
+        var player = new Player();
+        player.on('play', () => { // play 就是事件
+            console.log('正在播放');
+        });
+        player.emit('play');
 // 🍌🍌🍌 事件的参数<br />
-player.on('play2', (track) => {
-    console.log(`正在播放：${track}`);
-});
-player.emit('play2', '野子');
+        player.on('play2', (track) => {
+            console.log(`正在播放：${track}`);
+        });
+        player.emit('play2', '野子');
 // 🍌🍌🍌 只执行一次的事件监听器<br />
-player.once('play3', (track) => { // once 只执行一次的事件监听器
-    console.log(`正在播放：${track}`);
-});
-player.emit('play3', '野子');
-player.emit('play3', '青花');
+        player.once('play3', (track) => { // once 只执行一次的事件监听器
+            console.log(`正在播放：${track}`);
+        });
+        player.emit('play3', '野子');
+        player.emit('play3', '青花');
 // 🍠 【File System】<br />
 // 🍌🍌🍌 得到文件与目录的信息：stat<br />
-const fs = require('fs');
-fs.stat('index.js', (error, stats) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(stats);
-        console.log(`是否是文件：${stats.isFile()}`);
-        console.log(`是否是目录：${stats.isDirectory()}`);
-    }
-});
+        const fs = require('fs');
+        fs.stat('index.js', (error, stats) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(stats);
+                console.log(`是否是文件：${stats.isFile()}`);
+                console.log(`是否是目录：${stats.isDirectory()}`);
+            }
+        });
 // 🍌🍌🍌 创建一个目录：mkdir<br />
+        //
 // 🍌🍌🍌 创建文件并写入内容：writeFile, appendFile<br />
+        //
 // 🍌🍌🍌 读取文件里的内容：readFile<br />
+        //
 // 🍌🍌🍌 列出目录里的东西：readdir<br />
+        //
 // 🍌🍌🍌 重命名目录或文件：rename<br />
+        //
 // 🍌🍌🍌 删除目录与文件：rmdir, unlink<br />
+        //
 // 🍠 【Stream】<br />
 // 🍌🍌🍌 读取文件流<br />
 const fs2 = require('fs');
@@ -130,31 +136,36 @@ fileReadStream.on('data', (chunk) => {
     console.log(`${++count} 接收到：${chunk.length}`);
 });
 // 🍌🍌🍌 可读流的事件<br />
+        //
 // 🍌🍌🍌 可写的文件流<br />
+        //
 // 🍌🍌🍌 pipe<br />
+        //
 // 🍌🍌🍌 链式使用 pipe<br />
+        //
 // 🍠 【HTTP】<br />
 // 🍌🍌🍌 request<br />
-const http = require('http');
-var options = {
-    protocol: 'http:',
-    hostname: 'api.douban.com',
-    port: '80',
-    method: 'GET',
-    path: '/v2/movie/top250'
-};
-var request2 = http.request(options, (response) => {
-    console.log(response);
-    console.log(response.statusCode);
-    console.log(response.headers);
-});
-request2.on('error', (error) =>{
-    console.log(error);
-});
-request2.end();
+        const http = require('http');
+        var options = {
+            protocol: 'http:',
+            hostname: 'api.douban.com',
+            port: '80',
+            method: 'GET',
+            path: '/v2/movie/top250'
+        };
+        // var request2 = http.request(options, (response) => {
+        //     console.log(response);
+        //     console.log(response.statusCode);
+        //     console.log(response.headers);
+        // });
+        // request2.on('error', (error) =>{
+        //     console.log(error);
+        // });
+        // request2.end();
 // 🍌🍌🍌 利用请求回来的数据<br />
+        //
 // 🍌🍌🍌 创建服务器<br />
-//
+        //
 // 🌰🌰🌰🌰🌰🌰🌰🌰🌰 03 Node.js js：测试 🌰🌰🌰🌰🌰🌰🌰🌰🌰<br />
 // 🍠 【准备】<br />
 // 🌰🌰🌰 安装与准备<br />
