0a25426f31d8d69a9ac6f3a84f66228e588c7d50
howbuy2017 <15000001404@163.com>
2018-04-09 13:54:46 +0800

    photoshop初始结构

--- others/redux/chenfeng-redux/src/App.js
+++ others/redux/chenfeng-redux/src/App.js
@@ -8,10 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React(Redux)</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-intro">
+          Redux.
         </p>
       </div>
     );
--- others/redux/note.md
+++ others/redux/note.md
@@ -8,11 +8,18 @@
         yarn add redux
     **运行**
         yarn start
+
 *Redux*
 #State，Store，Action，Reducer
+
 #创建 Store 与 Reducer
+
 #Store 上的方法：getState，subscribe，dispatch
+
 #组合 Reducer
+
 #Reducer 是纯函数
+
 #指派动作：dispatch
+
 #用 combineReducers 组合 Reducer
